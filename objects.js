40426352f2b1a5fca6a2498905eca7d6d6f19dbd
David Thai <david.thai@dafuloth.me>
2018-08-28 05:41:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+<<<<<<< HEAD
   return playlist
+=======
+  return playlist 
+>>>>>>> 4a22d6a38b420372ae8e27ca9ba0e228fa0cf5fb
 }
